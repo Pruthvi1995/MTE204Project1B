f17e9eba1e5100a472e0c604529ed7f0b68286e8
Pruthvi1995 <pruthvi.atodaria@gmail.com>
2015-10-16 23:20:49 -0400

    changes to ABCD_tilda.m

--- ABCD_tilda.m
+++ ABCD_tilda.m
@@ -3,4 +3,4 @@ function [A_t, B_t, C_t, D_t] = ABCD_tilda(M, K, C, beta, gamma, deltat)
 A_t = (2/(beta*deltat^2))*M + K + ((2*gamma)/(beta*deltat))*C;
 B_t = (2/(beta*deltat^2))*M + ((2*gamma)/(beta*deltat))*C;
 C_t = (2/(beta*deltat))*M + (((2*gamma)/beta) + 1)*C;
-D_t = ((1-beta)/beta)*M + deltat*C*((gamma-1) + ((1-beta)/beta)*gamma)
+D_t = ((1-beta)/beta)*M + deltat*C*((gamma-1) + ((1-beta)/beta)*gamma);
